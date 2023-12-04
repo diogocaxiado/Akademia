3a810cc4ab215aedc9759354db92550d409eb565
DIOGO EVANGELISTA CAXIADO <diogo.ecaxiado@senacsp.edu.br>
2023-12-04 11:53:00 -0300

    Mudança no limite do varchar da procedure

--- db/dump.sql
+++ db/dump.sql
@@ -52,17 +52,18 @@ CREATE PROCEDURE piUsuario (
     IN _email			VARCHAR(100),
     IN _dtNascimento	DATE,
     IN _cidade			VARCHAR(100),
-    IN _senha			VARCHAR(100)
+    IN _senha			VARCHAR(50)
 )
 BEGIN
-	INSERT INTO usuario (nome, email, dtNascimento, cidade, senha) VALUES (_nome, _email, _dtNascimento , _cidade, _senha);
+	INSERT INTO usuario (nome, email, dtNascimento, cidade, senha) 
+    VALUES (_nome, _email, _dtNascimento , _cidade, _senha);
 END //
 
 DELIMITER // 
 
 CREATE PROCEDURE psLoginUsuario(
 	IN _email	VARCHAR(100),
-    IN _senha	VARCHAR(100)
+    IN _senha	VARCHAR(50)
 )
 
 BEGIN

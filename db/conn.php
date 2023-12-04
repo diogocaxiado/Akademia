58bc3108ec4a3d6c3b6eb2bf68b94df537b33ecf
DIOGO EVANGELISTA CAXIADO <diogo.ecaxiado@senacsp.edu.br>
2023-12-04 09:51:42 -0300

    Mudança na conexão do banco

--- db/conn.php
+++ db/conn.php
@@ -1,15 +1,27 @@
 <?php
-    $dbHost     = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName     = "akademia";
+    // $dbHost     = "localhost";
+    // $dbUsername = "root";
+    // $dbPassword = "";
+    // $dbName     = "akademia";
 
-    $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('DBNAME', 'akademia');
+    define('PORT', '3310');
 
-    $link = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    try{
+        $conn = new pdo('mysql:host=' . HOST . ';port=' . PORT . ';dbname=' . DBNAME, USER, PASS);
 
-    if ($link->connect_error) {
-        die("Connection failed: " . $db->connect_error);
-}
+    } catch(exception $err){
+        echo("<p>Erro ao conectar!" . $err->getCode() . "</p>");
+    }
 
+    // $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
+
+    // $link = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    // if ($link->connect_error) {
+    //     die("Connection failed: " . $db->connect_error);
+    // }
 ?>

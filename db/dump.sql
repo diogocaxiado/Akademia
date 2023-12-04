634c8a14cd20672cb41ad87d698386110ff1f0ba
DIOGO EVANGELISTA CAXIADO <diogo.ecaxiado@senacsp.edu.br>
2023-12-04 09:52:27 -0300

    Salvando as criações de tabela

--- db/dump.sql
+++ db/dump.sql
@@ -1,3 +1,50 @@
+-- CREATE 
+
+CREATE TABLE usuario (
+	idUsuario		INT PRIMARY KEY AUTO_INCREMENT,  
+    nome			VARCHAR(100) not null,
+    email			VARCHAR(100) not null,
+    dtNascimento	DATE not null,
+    cidade			VARCHAR(100) not null,
+    senha			VARCHAR(50) not null
+);
+
+CREATE TABLE modalidade (
+  	idModalidade	INT PRIMARY KEY AUTO_INCREMENT,
+    nome			VARCHAR(100) not null,
+    descricao		VARCHAR(100) not null,
+    imagem			VARCHAR(300) not null
+);
+
+CREATE TABLE plano (
+  	idPlano			INT PRIMARY KEY AUTO_INCREMENT,
+    nome			VARCHAR(100) not null,
+    descricao		VARCHAR(100) not null,
+    valor			DECIMAL(10,2) not null
+);
+
+CREATE TABLE itemPlano (
+  	idItemPlano		INT PRIMARY KEY AUTO_INCREMENT,
+    idModalidade	INT,
+    idPlano			INT, 
+    
+    CONSTRAINT fkItemPlanoModalidade FOREIGN KEY (idModalidade) REFERENCES modalidade (idModalidade),
+    CONSTRAINT fkItemPlanoPlano FOREIGN KEY (idPlano) REFERENCES plano (idPlano)
+);
+
+CREATE TABLE matricula (
+  	idMatricula		INT PRIMARY KEY AUTO_INCREMENT,
+    dtInicio		DATE not null,
+    dtValidade		DATE not null,
+    idPlano			INT,
+    idUsuario		INT,
+    
+    CONSTRAINT fkMatriculaPlano FOREIGN KEY (idPlano) REFERENCES plano (idPlano),
+    CONSTRAINT fkMatriculaUsuario FOREIGN KEY (idUsuario) REFERENCES usuario (idUsuario)
+);
+
+-- PROCEDURES
+
 DELIMITER //
 
 CREATE PROCEDURE piUsuario (
